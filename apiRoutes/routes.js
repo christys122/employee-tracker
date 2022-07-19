537bf24280a671cf590ed0e54561d5d65074df4e
unknown <mcsalazar122@gmail.com>
2022-07-18 20:39:10 -0500

    Routes complete

--- apiRoutes/routes.js
+++ apiRoutes/routes.js
@@ -4,7 +4,7 @@ const db = require('../db/connection');
 
 
 
-//SELECT ALL Query
+//SELECT ALL Employees Query
 router.get('/employees', (req, res) => {
     const sql = `SELECT employees.*,roles.job_title
                 AS Role
@@ -23,7 +23,7 @@ router.get('/employees', (req, res) => {
   });
    });
   
-  //SELECT ONE Query
+  //SELECT ONE employee by Id
   router.get('/employees/:id', (req, res) => {
     const sql = `SELECT employees.*, roles.job_title
                 AS Role
@@ -44,7 +44,7 @@ router.get('/employees', (req, res) => {
     })
   });
   
-  //DELETE ONE Query by Id
+  //DELETE ONE Employee by Id
   router.delete('/employees/:id', (req, res) => {
     const sql = `DELETE FROM employees WHERE id = ?`;
     const params = [req.params.id];
@@ -63,7 +63,7 @@ router.get('/employees', (req, res) => {
   });
   
   
-  //CREATE ONE Query New
+  //CREATE ONE New employee
   router.post('/employees', ({ body }, res) => {
   const sql = `INSERT INTO employees(first_name, last_name, hire_date, role_id, manager_id)
           VALUES (?,?,?,?,?)`;
@@ -79,38 +79,7 @@ router.get('/employees', (req, res) => {
       });
     });
   }); 
-  
-  //START DEPT Queries
-  
-  router.get('/departments', (req, res) => {
-    const sql = `SELECT * departments`;
-    db.query(sql, (err, rows) => {
-      if (err) {
-        res.status(500).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: rows
-      });
-    });
-  });
-  
-  router.get('/departments/:id', (req, res) => {
-    const sql = `SELECT * FROM departments WHERE id = ?`;
-    const params = [req.params.id];
-    db.query(sql, params, (err, row) => {
-      if (err) {
-        res.status(400).json({ error: err.message });
-        return;
-      }
-      res.json({
-        message: 'success',
-        data: row
-      });
-    });
-  });
-  
+
   //update employee role
   router.put('/employees/:id', (req, res) => {
     const sql = `UPDATE employees SET role_id = ?
@@ -133,7 +102,96 @@ router.get('/employees', (req, res) => {
       }
       });
     });
+  
+  //START DEPT Queries
+  //all departments
+  router.get('/departments', (req, res) => {
+    const sql = `SELECT * FROM departments`;
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
 
+  // SELECT one department
+  router.get('/departments/:id', (req, res) => {
+    const sql = `SELECT * FROM departments WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+  });
+
+   //Add New department
+   router.post('/departments', ({ body }, res) => {
+    const sql = `INSERT INTO departments(department_name)
+            VALUES (?)`;
+    const params = [body.department_name];
+    db.query(sql, params, (err, result) => {
+      if(err) {
+        res.status(400).json({ error: err.message });
+          return;
+        }
+        res.json({
+          message: 'success',
+          data: body
+        });
+      });
+    }); 
+  
+  
+  
+
+    //Roles Queries
+
+    //SELECT ALL Roles Query
+router.get('/roles', (req, res) => {
+  const sql = `SELECT roles.*,departments.department_name
+              AS Department
+              FROM roles
+              LEFT JOIN departments
+              ON roles.dept_id = departments.id`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+res.status(500).json({ error: err.message });
+    return;
+  }
+res.json({
+    message: 'success',
+    data: rows
+  });
+});
+ });
+
+    //Add New role
+    router.post('/roles', ({ body }, res) => {
+      const sql = `INSERT INTO roles(job_title, salary, dept_id)
+              VALUES (?,?,?)`;
+      const params = [body.job_title, body.salary, body.dept_id];
+      db.query(sql, params, (err, result) => {
+        if(err) {
+          res.status(400).json({ error: err.message });
+            return;
+          }
+          res.json({
+            message: 'success',
+            data: body
+          });
+        });
+      }); 
 
 
 
